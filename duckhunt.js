21044d12aae7a12242473b85757f79590e642203
hamoudsa <halqahta2007@my.fit.edu>
2018-11-25 02:22:20 +0300

    duckhunt.js

--- duckhunt.js
+++ duckhunt.js
@@ -3,15 +3,27 @@ $(document).ready(function() {
   // - jQuery will call it when the page is loaded
 
   // first lets grab the <body></body>
-  var body = $('body');
+  var $body = $('body');
 
   // 1. Can you create a <div> with the class "duck" and name it "duck"
 
+  var $duck = $('<div/>').addClass('duck');
+
+$body.append($duck);
+
+  
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
-
+  setInterval( function(){
+    $duck.toggleClass('flap');
+}, 250);
   // 3. Fantastic!  Now, let's move the duck using CSS "top" and "left"
-
+  $duck.css ({ 'left' : '80px', 'top' : '80px'}, 5000, function(){
+    $duck.fadeOut(3000);
+ } );
+  
   // 4. Try making the duck move to a different location after 1 second
-
+   /* function fadeTechIn() {
+    console.log('hi');
+    $duck.fadeIn(2000);  */
   // 5. Congratulations!
 })
