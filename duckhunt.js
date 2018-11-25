6923d2b5dbab84f67461fcb3d9a6fabd150ebf88
hamoudsa <halqahta2007@my.fit.edu>
2018-11-25 08:50:32 +0300

    duckhunt

--- duckhunt.js
+++ duckhunt.js
@@ -17,13 +17,43 @@ $body.append($duck);
     $duck.toggleClass('flap');
 }, 250);
   // 3. Fantastic!  Now, let's move the duck using CSS "top" and "left"
-  $duck.css ({ 'left' : '80px', 'top' : '80px'}, 5000, function(){
+  $duck.css ({ left : '80px', top : '80px'}, 5000, function(){
     $duck.fadeOut(3000);
  } );
   
   // 4. Try making the duck move to a different location after 1 second
-   /* function fadeTechIn() {
-    console.log('hi');
-    $duck.fadeIn(2000);  */
-  // 5. Congratulations!
+  /* setInterval( function(){
+      var toRight = Math.floor(Math.random()) * window.innerWidth;
+      var toLeft = Math.floor(Math.random()) * window.innerHeight;})
+    $duck.animate({
+      Right : `${toRight}px`,
+      Left : `${toLeft}px`
+    },2000);
+  }); */
+  function makeNewPosition(){
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+ 
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+ 
+    return [nh,nw];
+ 
+ }
+ 
+ function animateDiv(newPo){
+    var newq = makeNewPosition();
+    $(newPo).animate({ top: newq[0], left: newq[1] }, 2000,   function(){
+      animateDiv(newPo);
+    });
+ 
+ };
 })
+   
+  // 5. Congratulations!
+$createDu
+
+
+
